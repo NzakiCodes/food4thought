e1b39bf5c07a0e4f1eff7b04b74338bbf840fa5c
Nzaki Michael <ekisnzaki@gmail.com>
2022-03-05 22:27:14 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif:['Poppins', 'sans-serif'],
+        brand:['Pacifico', 'cursive'],
+      },
+      colors:{
+        'primary':'#f86e65',
+        'secondary':'#fb6b9e',
+      }
+    },
+  },
+  plugins: [],
+}
